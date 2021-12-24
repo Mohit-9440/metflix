12a26186e6ac860cd945a3d0bf000a71b903d60a
Mohit <mohitsharma8864@gmail.com>
2021-12-25 05:21:55 +0530

    firebase initial setup

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,3 +1,22 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { seedDatabase } from '../seed';
+
+// we need to somehow seed the database
+
+// we need a config here
+const config = {
+    apiKey: "AIzaSyDHFwndZSMlrJqWWaY09KsgMyf0Uan6RJY",
+    authDomain: "metflix-5bf44.firebaseapp.com",
+    projectId: "metflix-5bf44",
+    storageBucket: "metflix-5bf44.appspot.com",
+    messagingSenderId: "96206229669",
+    appId: "1:96206229669:web:572715c984cb68b36c3d4c"
+};
+
+const firebase = Firebase.initializeApp(config);
+
+seedDatabase(firebase);
+
+export { firebase };
