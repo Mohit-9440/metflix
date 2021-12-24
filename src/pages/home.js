1e0890ce6cc84d81f6981f6937d987c49ca4c93b
Mohit <mohitsharma8864@gmail.com>
2021-12-24 12:47:05 +0530

    optform issue

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { OptForm } from '../components'
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
