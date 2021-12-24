95894f120b6af4cfe8368ae9a8fb05f02092cf13
Mohit <mohitsharma8864@gmail.com>
2021-12-24 16:23:12 +0530

    header

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,13 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from "react";
 import { Header } from "../components";
+import ROUTES from '../constants/routes'
 
-export default function HeaderContainer() {
+export function HeaderContainer( {children} ) {
     return (
         <Header>
             <Header.Frame>
-                <Header.Logo />
-                <Header.Button />
+                <Header.Logo to={ROUTES.HOME} alt="Metflix"/>
+                <Header.Button to={ROUTES.SIGN_IN}>Sign In</Header.Button>
             </Header.Frame>
         </Header>
-    )
+    );
 }
